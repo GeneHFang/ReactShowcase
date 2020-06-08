12997b04133247ea1bae628dde5d44ed2d06bfa5
GeneHFang <gene.li@nyu.edu>
2020-06-07 22:12:14 -0400

    changed onChange to onClick

--- reactshowcase/src/components/Card.js
+++ reactshowcase/src/components/Card.js
@@ -7,8 +7,8 @@ const Card = (props) => {
             {props.info}
             <button onClick={()=>props.remove(props.col,props.index)}>Remove Card</button>
             <br/>
-            <button disabled={props.col === 1} onChange={()=>props.change(props.col,props.col-1,props.index)} >&lt;</button>
-            <button disabled={props.col === 3} onChange={()=>props.change(props.col,props.col+1,props.index)} >&gt;</button>
+            <button disabled={props.col === 1} onClick={()=>props.change(props.col,props.col-1,props.index)} >&lt;</button>
+            <button disabled={props.col === 3} onClick={()=>props.change(props.col,props.col+1,props.index)} >&gt;</button>
         </div>
     );
 };
