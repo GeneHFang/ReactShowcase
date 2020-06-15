a47644ec1e470122ec336495ea0f480970085780
GeneHFang <gene.li@nyu.edu>
2020-06-14 22:14:50 -0400

    added styling for cards

--- reactshowcase/src/components/Card.js
+++ reactshowcase/src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../css/Card.css';
 
 //Card component that is added to each column individually
 const Card = (props) => {
     return(
-        <div>
+        <div className="card">
             {props.info}
+            <br/>
             <button onClick={()=>props.remove(props.col,props.index)}>Remove Card</button>
             <br/>
             <button disabled={props.col === 1} onClick={()=>props.change(props.col,props.col-1,props.index)} >&lt;</button>
